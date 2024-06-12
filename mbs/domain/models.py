591b0d4189e66fd6ea3733c73cc9ecd8ec13ca71
samuel <sam0101dev@gmail.com>
2024-06-12 19:15:45 -0400

    feat: add business logic and data models

---
+++ mbs/domain/models.py
@@ -0,0 +1,64 @@
+
+
+class User:
+
+    def __init__(self, cedula, name, last_name, cell_phone, email, address, role, date_born=None, province=None, electoral_college=None, electoral_college_location=None):
+        self.cedula = celula
+        self.name = name
+        self.last_name = last_name
+        self.cell_phone = cell_phone
+        self.email = email
+        self.address = address
+        self.role = role
+        self.date_born = date_born
+        self.province = province
+        self.electoral_college
+        self.electoral_college_location
+
+        def __str__(self):
+            return f"{self.cedula} {self.name} {self.last_name}"
+
+
+class Coordinator(User):
+
+    def __init__(self, cedula, name, last_name, cell_phone, email, address, role, date_born=None, province=None, electoral_college=None, electoral_college_location=None):
+        super().__init__(cedula, name, last_name, cell_phone, email, address,
+                         'coordinator', date_born, province, electoral_college, electoral_college_location)
+        self.facilitators = []
+        self.member_manager = MemberManager()
+
+    def add_facilitator(self, facilitator):
+
+        if len(self.facilitators) < 15:
+            self.facilitators.append(facilitator)
+        else:
+            raise ValueError(
+                'A coordinator cannot have more than 15 facilitators')
+
+    def add_member(self, member):
+        self.member_manager.add_member(member)
+
+
+class Facilitator(User):
+
+    def __init__(self, cedula, name, last_name, cell_phone, email, address, coordinator, role, date_born=None, province=None, electoral_college=None, electoral_college_location=None):
+        super.__init__(cedula, name, last_name, cell_phone, email, address,
+                       'facilitator', date_born, province, electoral_college, electoral_college_location)
+        self.coordinator = coordinator
+        self.member_manager = MemberManager()
+
+    def add_member(self, member):
+        self.member_manager.add_member(member)
+
+
+class MemberManager:
+
+    def __init__(self):
+        self.members = []
+
+    def add_member(self, member):
+
+        if len(self.members) < 15:
+            self.members.append(member)
+        else:
+            raise ValueError('Cannot have more than 15 members')
--- mbs/infrastructure/models.py
+++ mbs/infrastructure/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class ElectoralRoll(models.Model):
+    cedula = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_born = models.DateField()
+    province = models.CharField(max_length=50)
+    electoral_college = models.CharField(max_length=7)
+    electoral_college_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f" {self.cedula} {self.name} {self.lastname}"
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('superadmin', 'Super Administrator'),
+        ('coordinator', 'Coordinator'),
+        ('facilitator', 'Facilitator')
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    cedula = models.CharField(max_length=15, unique=True)
+    date_born = models.DateField()
+    province = models.CharField(max_length=50)
+    electoral_college = models.CharField(max_length=7)
+    electoral_college_location = models.CharField(max_length=100)
